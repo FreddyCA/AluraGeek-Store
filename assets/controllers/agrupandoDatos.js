0b6e0936db49bdfc80bdac0b003cf857aaa72ea5
Freddy Coaricona <fcoaricona@outlook.com>
2023-04-12 20:24:00 -0400

    session storage, editar datos

--- assets/controllers/agrupandoDatos.js
+++ assets/controllers/agrupandoDatos.js
@@ -1,28 +1,32 @@
 const seleccionandoDatos = () => {
     const editar = document.querySelectorAll(".producto__herramienta--editar")
     editar.forEach(element => {
-        element.addEventListener("mouseover", (e) => {
+        element.addEventListener("click", async (e) => {
             const elementoMolde = e.target.parentNode.parentNode
-            extrayendoData(elementoMolde)
+            await extrayendoData(elementoMolde)
         })
     });
 }
 
-let nombreProducto = null
+let estado = false
 
 const extrayendoData = async (elementoMolde) => {
     const elementosGrupo = elementoMolde.children[1].children
     const nombre = elementosGrupo[0].textContent
     const precio = elementosGrupo[1].textContent
     const descripcion = elementosGrupo[2].textContent
+    estado = true
     // console.log(nombre, precio, descripcion)
-    nombreProducto = nombre
-    console.log(nombreProducto)
-    
+    await agregandoAlStorage(nombre, precio, descripcion, estado)
 }
 
+const agregandoAlStorage = async (nombre, precio, descripcion, estado) => {
+    sessionStorage.setItem('nombre', nombre)
+    sessionStorage.setItem('precio', precio)
+    sessionStorage.setItem('descripcion', descripcion)
+    sessionStorage.setItem('estado', estado)
+}
 
 export const paraEditar = {
     seleccionandoDatos,
-    nombreProducto
 }
--- assets/controllers/nuevoProducto.js
+++ assets/controllers/nuevoProducto.js
@@ -1,4 +1,27 @@
-console.log("hola")
-import { paraEditar } from "./agrupandoDatos.js"
+console.log('first')
 
-console.log(paraEditar.nombreProducto)
+const nombre = sessionStorage.getItem('nombre')
+const precio = sessionStorage.getItem('precio')
+const descripcion = sessionStorage.getItem('descripcion')
+console.log(nombre, precio, descripcion)
+
+const inputNombre = document.getElementById("name")
+const inputPrecio = document.getElementById("precio")
+const inputDescripcion = document.getElementById("descripcion")
+const agregarProductoBtn = document.querySelector(".addProducto__btn")
+
+const datosCargado =  () => {
+    inputNombre.value = nombre
+    inputPrecio.value = precio
+    inputDescripcion.value = descripcion
+
+}
+document.addEventListener("DOMContentLoaded",  () => {
+     datosCargado()
+})
+
+agregarProductoBtn.addEventListener("cick", () => {
+    sessionStorage.clear()
+    console.log(inputNombre.value)
+
+})
